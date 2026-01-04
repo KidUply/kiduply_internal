5da9f1dab8551d3ccfb8b32e6c271e341debdb25
Muhammadyusuf <kiduply@gmail.com>
2026-01-04 08:00:43 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
-  images: { 
-    unoptimized: true 
-  },
-  // Build to'xtab qolmasligi uchun xatolarni o'tkazib yuboramiz
-  typescript: { 
-    ignoreBuildErrors: true 
-  },
-  eslint: { 
-    ignoreDuringBuilds: true 
+  images: {
+    unoptimized: true,
   },
 };
 
